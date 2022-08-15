312d672632b7737b44d6df241a12d6ac004ccc25
Андрей <temp60@mail.ru>
2022-08-15 07:36:29 +0300

    Наконец-то смог имплементировать format()

--- homework.py
+++ homework.py
@@ -18,10 +18,10 @@ class InfoMessage:
 
     def get_message(self) -> str:
         return (
-            'Тип тренировки: {training_type};'
-            'Длительность: {duration:.3f} ч;'
+            'Тип тренировки: {training_type}; '
+            'Длительность: {duration:.3f} ч.; '
             'Дистанция: {distance:.3f} км; '
-            'Ср. скорость: {speed:.3f} км/ч;'
+            'Ср. скорость: {speed:.3f} км/ч; '
             'Потрачено ккал: {calories:.3f}.'.format(**asdict(self))
         )
 
